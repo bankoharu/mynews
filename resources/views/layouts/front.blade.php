0493d98dc7a7c1f35b4a180ea429f2f67977f8ea
koharu shimoji <k0123610@gmail.com>
2021-01-29 09:16:31 +0000

    heroku課題

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -45,6 +45,7 @@
                         {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
                         @guest
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            <p>ログインしてください</p>
                         {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
                         @else
                             <li class="nav-item dropdown">
