7365f586201d3dee05c83c2a1b9ad521218f1b63
koharu shimoji <k0123610@gmail.com>
2021-01-30 05:48:08 +0000

    debug解除解除

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -82,7 +82,7 @@ class ProfileController extends Controller
      public function delete(Request $request)
   {
       // 該当するNews Modelを取得
-      $news = Profile::find($request->id);
+      $profile = Profile::find($request->id);
       // 削除する
       $profile->delete();
       return redirect('admin/profile/');
