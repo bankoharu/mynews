acef26d68b61a9195130f3608597f2e68824988e
koharu shimoji <k0123610@gmail.com>
2021-02-14 11:00:35 +0000

    layout_base

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -73,7 +73,9 @@
              
              <main class="py-4">
                  {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
+                 
                 @yield('content')
+                
              </main>
         </div>
     </body>
